d5ce1ef4dc9000212c17b3d64b695a546b44c292
Matthew Rittinghouse <rittinghousemt@g.cofc.edu>
2023-08-10 21:43:03 -0400

    Updating codebase.

---
+++ src/dfs_optimizer.py
@@ -0,0 +1,176 @@
+import pandas as pd
+import json
+from itertools import product
+from typing import List, Dict, Union
+from dataclasses import dataclass
+
+
+@dataclass
+class RosterOptimizer:
+    dk: pd.DataFrame
+    fade_list: List[str]
+    salary_cap: int = 1500000
+    max_team_size: int = 3
+
+    def prep_dk_csv(self) -> Dict[str, List[str]]:
+        """
+        Prepare the DraftKings CSV data.
+        Transforms raw data from DraftKings and formats it for the optimizer.
+
+        Returns:
+            dict: A dictionary containing match data.
+        """
+        self.dk["Name"] = self.dk["Name"].str.strip()
+        self.dk = self.dk[~self.dk["Name"].isin(self.fade_list)]
+        match_list = self._generate_match_list(self.dk["Game Info"].unique())
+        return self._generate_match_evaluation_string(match_list)
+
+    @staticmethod
+    def _generate_match_list(raw_match_list: List[str]) -> List[str]:
+        """
+        Generate a list of matches from the raw match data.
+
+        Args:
+            raw_match_list (List[str]): Raw match list to process.
+        Returns:
+            List[str]: Processed match list.
+        """
+        match_list = []
+        for match in raw_match_list:
+            match = match.split(" ", 1)[0]
+            match = match.replace("@", '" , "')
+            match = '["' + match + '"]'
+            match_list.append(match)
+        return match_list
+
+    @staticmethod
+    def _generate_match_evaluation_string(
+        match_list: List[str],
+    ) -> Dict[str, List[str]]:
+        """
+        Generate a match evaluation string from the list of matches.
+
+        Args:
+            match_list (List[str]): List of matches.
+        Returns:
+            dict: Evaluation string for matches.
+        """
+        match_string = "{"
+        for i, match in enumerate(match_list, start=1):
+            string = f'"game{i}": {match},'
+            match_string += string
+        match_string = match_string.rstrip(",") + "}"
+        return json.loads(match_string)
+
+    def initialize_rosters(self) -> Dict[str, pd.DataFrame]:
+        """
+        Initialize roster dataframes based on positions.
+
+        Returns:
+            dict: Dictionary of dataframes for each position.
+        """
+        positions = ["Top", "Jng", "Mid", "ADC", "Sup", "Team"]
+        rosters = {
+            pos.lower(): self.dk[self.dk["Position"] == pos].reset_index(drop=True)
+            for pos in positions
+        }
+        return rosters
+
+    def generate_individual_roster(
+        self, rosters: Dict[str, pd.DataFrame]
+    ) -> Dict[str, Union[str, int]]:
+        """
+        Generate an individual roster based on constraints.
+
+        Args:
+            rosters (Dict[str, pd.DataFrame]): Dictionary of dataframes
+                                                for each position.
+        Returns:
+            dict: The best roster based on the given constraints.
+        """
+        best_roster = {
+            "Top": "",
+            "Jng": "",
+            "Mid": "",
+            "ADC": "",
+            "Sup": "",
+            "Team": "",
+            "Cost": 0,
+            "Score": 0,
+        }
+
+        # Using itertools product to simplify nested loops
+        for top, jng, mid, adc, sup, team in product(
+            rosters["top"].iterrows(),
+            rosters["jng"].iterrows(),
+            rosters["mid"].iterrows(),
+            rosters["adc"].iterrows(),
+            rosters["sup"].iterrows(),
+            rosters["team"].iterrows(),
+        ):
+            _, top = top
+            _, jng = jng
+            _, mid = mid
+            _, adc = adc
+            _, sup = sup
+            _, team = team
+
+            ind_cost = (
+                top["Cost"]
+                + jng["Cost"]
+                + mid["Cost"]
+                + adc["Cost"]
+                + sup["Cost"]
+                + team["Cost"]
+            )
+            if ind_cost > self.salary_cap:
+                continue
+
+            ind_score = (
+                top["Pts"]
+                + jng["Pts"]
+                + mid["Pts"]
+                + adc["Pts"]
+                + sup["Pts"]
+                + team["Pts"]
+            )
+            if ind_score <= best_roster["Score"]:
+                continue
+
+            # Check team count constraint
+            teams = [
+                top["team"],
+                jng["team"],
+                mid["team"],
+                adc["team"],
+                sup["team"],
+                team["team"],
+            ]
+            if max([teams.count(t) for t in set(teams)]) > self.max_team_size:
+                continue
+
+            best_roster = {
+                "Top": f"{top['team']} {top['player']}",
+                "Jng": f"{jng['team']} {jng['player']}",
+                "Mid": f"{mid['team']} {mid['player']}",
+                "ADC": f"{adc['team']} {adc['player']}",
+                "Sup": f"{sup['team']} {sup['player']}",
+                "Team": f"{team['team']} {team['player']}",
+                "Cost": ind_cost,
+                "Score": ind_score,
+            }
+
+        del best_roster["Score"]
+
+        if all(value == "" or value == 0 for value in best_roster.values()):
+            raise Exception("No mathematically possible rosters for this stack.")
+        else:
+            return best_roster
+
+    def optimize(self) -> Dict[str, Union[str, int]]:
+        """Optimize and generate the best possible roster.
+        Returns:
+            dict: Best possible roster based on constraints.
+        """
+        rosters = self.initialize_rosters()
+        return self.generate_individual_roster(rosters)
--- src/oracles_elixir.py
+++ src/oracles_elixir.py
@@ -8,6 +8,7 @@ to leverage pro game data for use in their own scripts and analytics.
 Please visit and support www.oracleselixir.com
 Tim provides an invaluable service to the League community.
 """
+from dataclasses import dataclass
 import datetime as dt
 from typing import Dict, Optional, Union
 
@@ -24,22 +25,17 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s", datefmt=date_format
 )
 logger = logging.getLogger(__name__)
-logger.setLevel('INFO')
+logger.setLevel("INFO")
 
 
 # Primary Functions
+@dataclass
 class OraclesElixir:
-    def __init__(self, session: Optional[boto3.Session], bucket: str):
-        """
-        bucket : str
-            Name of the bucket containing Oracle's Elixir S3 data.
-        """
-        self.oe_data = pd.DataFrame()
-        self.session = session
-        self.bucket = bucket
+    session: Optional[boto3.Session]
+    bucket: str
 
     def ingest_data(
-        self, years: Optional[Union[list, str, int]] = [dt.date.today().year]
+        self, years: Optional[Union[list, str, int]] = None
     ) -> pd.DataFrame:
         """
         Pull data from S3 based on the specified years
@@ -51,6 +47,8 @@ class OraclesElixir:
             A string or list of strings containing years (e.g. ["2019", "2020"]).
             If nothing is specified, returns the current year only by default.
         """
+        if years is None:
+            years = [dt.date.today().year]
         if isinstance(years, (str, int)):
             years = [years]
 
@@ -59,9 +57,11 @@ class OraclesElixir:
             for year in years
         ]
         logger.info(file_paths)
-        self.oe_data = wr.s3.read_csv(file_paths, boto3_session=self.session, low_memory=False)
+        oe_data = wr.s3.read_csv(
+            file_paths, boto3_session=self.session, low_memory=False
+        )
 
-        return self.oe_data
+        return oe_data
 
     @staticmethod
     def get_opponent(column: pd.Series, entity: str) -> list:
@@ -91,11 +91,9 @@ class OraclesElixir:
         # The gap represents how many rows separate a value from its opponent
         # Teams are 1 (Team A, Team B)
         # Players are 5 (ADC to opposing ADC is a 5 row gap)
-        if entity == "player":
-            gap = 5
-        elif entity == "team":
-            gap = 1
-        else:
+        gap_dict = {"player": 5, "team": 1}
+        gap = gap_dict.get(entity)
+        if gap is None:
             raise ValueError("Entity must be either player or team.")
 
         for i, obj in enumerate(column):
@@ -115,12 +113,13 @@ class OraclesElixir:
                 flag = 0
         return opponent
 
-    def format_data_types(self) -> pd.DataFrame:
-        self.oe_data["date"] = pd.to_datetime(self.oe_data["date"])
+    @staticmethod
+    def format_data_types(oe_data: pd.DataFrame) -> pd.DataFrame:
+        oe_data["date"] = pd.to_datetime(oe_data["date"])
         f_cols = ["gameid", "playerid", "teamid"]
-        self.oe_data[f_cols] = self.oe_data[f_cols].apply(lambda x: x.str.strip())
+        oe_data[f_cols] = oe_data[f_cols].apply(lambda x: x.str.strip())
         replace_values = {"": np.nan, "nan": np.nan, "null": np.nan}
-        self.oe_data = self.oe_data.replace(
+        oe_data = oe_data.replace(
             {
                 "gameid": replace_values,
                 "playerid": replace_values,
@@ -128,152 +127,161 @@ class OraclesElixir:
                 "position": replace_values,
             }
         )
-        return self.oe_data
+        return oe_data
 
-    def remove_null_games(self) -> pd.DataFrame:
-        return self.oe_data.dropna(subset=["gameid"])
+    @staticmethod
+    def remove_null_games(oe_data: pd.DataFrame) -> pd.DataFrame:
+        return oe_data.dropna(subset=["gameid"])
 
-    def drop_unknown_entities(self) -> pd.DataFrame:
-        return self.oe_data[
-            (~self.oe_data["playername"].isin(["unknown player"]))
-            & (~self.oe_data["teamname"].isin(["unknown team"]))
+    @staticmethod
+    def drop_unknown_entities(oe_data: pd.DataFrame) -> pd.DataFrame:
+        return oe_data[
+            (~oe_data["playername"].isin(["unknown player"]))
+            & (~oe_data["teamname"].isin(["unknown team"]))
         ]
 
-    def drop_negative_earned_gpm(self) -> pd.DataFrame:
-        return self.oe_data[self.oe_data["earned gpm"] >= 0]
+    @staticmethod
+    def drop_negative_earned_gpm(oe_data: pd.DataFrame) -> pd.DataFrame:
+        return oe_data[oe_data["earned gpm"] >= 0]
 
+    @staticmethod
     def normalize_names(
-        self,
-        team_replacements: Dict,
-        player_replacements: Dict,
+        oe_data: pd.DataFrame,
+        team_replacements: Dict = None,
+        player_replacements: Dict = None,
     ) -> pd.DataFrame:
         if team_replacements:
-            self.oe_data["teamname"] = self.oe_data["teamname"].replace(
-                team_replacements
-            )
+            oe_data["teamname"] = oe_data["teamname"].replace(team_replacements)
         if player_replacements:
-            self.oe_data["playername"] = self.oe_data["playername"].replace(
-                player_replacements
-            )
-        return self.oe_data
+            oe_data["playername"] = oe_data["playername"].replace(player_replacements)
+        return oe_data
 
-    def subset_data(self, split_on: Optional[str]) -> pd.DataFrame:
-        if split_on == "team":
-            return self.oe_data[
-                [
-                    "date",
-                    "gameid",
-                    "side",
-                    "league",
-                    "teamname",
-                    "teamid",
-                    "result",
-                    "kills",
-                    "deaths",
-                    "assists",
-                    "earned gpm",
-                    "gamelength",
-                    "ckpm",
-                    "team kpm",
-                    "firstblood",
-                    "dragons",
-                    "barons",
-                    "towers",
-                    "goldat15",
-                    "xpat15",
-                    "csat15",
-                    "golddiffat15",
-                    "xpdiffat15",
-                    "csdiffat15",
-                ]
-            ]
-        elif split_on == "player":
-            return self.oe_data[
-                [
-                    "date",
-                    "gameid",
-                    "side",
-                    "position",
-                    "league",
-                    "playername",
-                    "playerid",
-                    "teamname",
-                    "teamid",
-                    "result",
-                    "kills",
-                    "deaths",
-                    "assists",
-                    "total cs",
-                    "earned gpm",
-                    "earnedgoldshare",
-                    "gamelength",
-                    "ckpm",
-                    "team kpm",
-                    "goldat15",
-                    "xpat15",
-                    "csat15",
-                    "killsat15",
-                    "assistsat15",
-                    "deathsat15",
-                    "opp_killsat15",
-                    "opp_assistsat15",
-                    "opp_deathsat15",
-                    "golddiffat15",
-                    "xpdiffat15",
-                    "csdiffat15",
-                ]
-            ]
+    @staticmethod
+    def subset_data(oe_data: pd.DataFrame, split_on: Optional[str]) -> pd.DataFrame:
+        columns = {
+            "team": [
+                "date",
+                "gameid",
+                "side",
+                "league",
+                "teamname",
+                "teamid",
+                "result",
+                "kills",
+                "deaths",
+                "assists",
+                "earned gpm",
+                "gamelength",
+                "ckpm",
+                "team kpm",
+                "firstblood",
+                "dragons",
+                "barons",
+                "towers",
+                "goldat15",
+                "xpat15",
+                "csat15",
+                "golddiffat15",
+                "xpdiffat15",
+                "csdiffat15",
+            ],
+            "player": [
+                "date",
+                "gameid",
+                "side",
+                "position",
+                "league",
+                "playername",
+                "playerid",
+                "teamname",
+                "teamid",
+                "result",
+                "kills",
+                "deaths",
+                "assists",
+                "total cs",
+                "earned gpm",
+                "earnedgoldshare",
+                "gamelength",
+                "ckpm",
+                "team kpm",
+                "goldat15",
+                "xpat15",
+                "csat15",
+                "killsat15",
+                "assistsat15",
+                "deathsat15",
+                "opp_killsat15",
+                "opp_assistsat15",
+                "opp_deathsat15",
+                "golddiffat15",
+                "xpdiffat15",
+                "csdiffat15",
+            ],
+        }
+        if split_on in columns:
+            if split_on.lower() == "team":
+                oe_data = oe_data[oe_data["position"] == "team"]
+            else:
+                oe_data = oe_data[oe_data["position"] != "team"]
+            return oe_data[columns[split_on]]
         else:
             raise ValueError("Must split on either player or team.")
 
-    def remove_inconsistent_games(self, split_on: Optional[str]) -> pd.DataFrame:
-        counts = self.oe_data["gameid"].value_counts()
+    @staticmethod
+    def remove_inconsistent_games(
+        oe_data: pd.DataFrame, split_on: Optional[str]
+    ) -> pd.DataFrame:
+        counts = oe_data["gameid"].value_counts()
         inconsistent_games = counts[
-            (counts != 2) if split_on == "team" else (counts < 1) | (counts > 10)
+            (counts != 2) if split_on == "team" else (counts != 10)
         ].index
-        return self.oe_data[~self.oe_data["gameid"].isin(inconsistent_games)]
+        return oe_data[~oe_data["gameid"].isin(inconsistent_games)]
 
-    def sort_data(self, split_on: Optional[str]) -> pd.DataFrame:
+    @staticmethod
+    def sort_data(oe_data: pd.DataFrame, split_on: Optional[str]) -> pd.DataFrame:
         if split_on == "player":
-            return self.oe_data.sort_values(
-                ["league", "date", "gameid", "side", "position"]
-            )
+            return oe_data.sort_values(["league", "date", "gameid", "side", "position"])
         elif split_on == "team":
-            return self.oe_data.sort_values(["league", "date", "gameid", "side"])
+            return oe_data.sort_values(["league", "date", "gameid", "side"])
 
-    def fill_null_team_ids(self, split_on: str) -> pd.DataFrame:
+    @staticmethod
+    def fill_null_team_ids(oe_data: pd.DataFrame, split_on: str) -> pd.DataFrame:
         if split_on == "player":
-            self.oe_data["teamid"] = self.oe_data["teamid"].fillna(
-                self.oe_data["teamname"]
-            )
-        return self.oe_data
+            oe_data["teamid"] = oe_data["teamid"].fillna(oe_data["teamname"])
+        return oe_data
 
-    def enrich_opponent_metrics(self, split_on: str) -> pd.DataFrame:
+    def enrich_opponent_metrics(
+        self, oe_data: pd.DataFrame, split_on: str
+    ) -> pd.DataFrame:
+        metrics = {
+            "teamid": oe_data["teamid"].fillna(oe_data["teamname"]),
+            "opponentteam": self.get_opponent(oe_data["teamname"].to_list(), split_on),
+            "opponentteamid": self.get_opponent(oe_data["teamid"].to_list(), split_on),
+            "opponent_egpm": self.get_opponent(
+                oe_data["earned gpm"].to_list(), split_on
+            ),
+        }
         if split_on == "player":
-            self.oe_data["playerid"] = self.oe_data["playerid"].fillna(
-                self.oe_data["playername"]
-            )
-            self.oe_data["opponentteam"] = self.get_opponent(
-                self.oe_data["teamname"].to_list(), split_on
-            )
-            self.oe_data["opponentteamid"] = self.get_opponent(
-                self.oe_data["teamid"].to_list(), split_on
+            metrics.update(
+                {
+                    "playerid": oe_data["playerid"].fillna(oe_data["playername"]),
+                    "opponentname": self.get_opponent(
+                        oe_data["playername"].to_list(), split_on
+                    ),
+                    "opponentid": self.get_opponent(
+                        oe_data["playerid"].to_list(), split_on
+                    ),
+                }
             )
 
-        self.oe_data["opponentname"] = self.get_opponent(
-            self.oe_data["playername"].to_list(), split_on
-        )
-        self.oe_data["opponentid"] = self.get_opponent(
-            self.oe_data["playerid"].to_list(), split_on
-        )
-        self.oe_data["opponent_egpm"] = self.get_opponent(
-            self.oe_data["earned gpm"].to_list(), split_on
-        )
+        oe_data = oe_data.assign(**metrics)
 
-        return self.oe_data
+        return oe_data
 
     def clean_data(
         self,
+        oe_data: pd.DataFrame,
         split_on: Optional[str],
         team_replacements: Optional[Dict] = None,
         player_replacements: Optional[Dict] = None,
@@ -291,6 +299,8 @@ class OraclesElixir:
 
         Parameters
         ----------
+        oe_data:
+            Pandas DataFrame containing Oracle's Elixir data.
         split_on : 'team', 'player' or None
             Subset data for Team data or Player data. None for all data.
         team_replacements: Optional[dict]
@@ -307,14 +317,14 @@ class OraclesElixir:
         A Pandas dataframe of formatted, subset Oracle's Elixir data matching
         the parameters provided above.
         """
-        self.oe_data = self.format_data_types()
-        self.oe_data = self.remove_null_games()
-        self.oe_data = self.drop_unknown_entities()
-        self.oe_data = self.drop_negative_earned_gpm()
-        self.oe_data = self.normalize_names(team_replacements, player_replacements)
-        self.oe_data = self.subset_data(split_on)
-        self.oe_data = self.remove_inconsistent_games(split_on)
-        self.oe_data = self.sort_data(split_on)
-        self.oe_data = self.fill_null_team_ids(split_on)
-        self.oe_data = self.enrich_opponent_metrics(split_on)
-        return self.oe_data
+        oe_data = self.format_data_types(oe_data)
+        oe_data = self.remove_null_games(oe_data)
+        oe_data = self.drop_unknown_entities(oe_data)
+        oe_data = self.drop_negative_earned_gpm(oe_data)
+        oe_data = self.sort_data(oe_data, split_on)
+        oe_data = self.fill_null_team_ids(oe_data, split_on)
+        oe_data = self.normalize_names(oe_data, team_replacements, player_replacements)
+        oe_data = self.subset_data(oe_data, split_on)
+        oe_data = self.remove_inconsistent_games(oe_data, split_on)
+        oe_data = self.enrich_opponent_metrics(oe_data, split_on)
+        return oe_data
