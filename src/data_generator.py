a351083ffecb9c450b77b19b629ca804d2b62139
Yureeh <fabbri.juri@gmail.com>
2025-01-10 16:20:40 +0100

    .

--- src/data_generator.py
+++ src/data_generator.py
@@ -297,25 +297,25 @@ class DataGenerator:
             # )
             # double_logging_call("Completed enriching data with Glicko2.\n")
 
-            # # (4) Plackett-Luce
-            # self.team_data, self.player_data = parallelize_enrichment(
-            #     self.rating_models.compute_plackett_luce,
-            #     self.team_data,
-            #     self.player_data,
-            #     "team",
-            #     "player",
-            # )
-            # double_logging_call("Completed enriching data with Plackett-Luce.\n")
-
-            # (5) TrueSkill
+            # (4) Plackett-Luce
             self.team_data, self.player_data = parallelize_enrichment(
-                self.rating_models.compute_trueskill,
+                self.rating_models.compute_plackett_luce,
                 self.team_data,
                 self.player_data,
                 "team",
                 "player",
             )
-            double_logging_call("Completed enriching data with TrueSkill.\n")
+            double_logging_call("Completed enriching data with Plackett-Luce.\n")
+
+            # # (5) TrueSkill
+            # self.team_data, self.player_data = parallelize_enrichment(
+            #     self.rating_models.compute_trueskill,
+            #     self.team_data,
+            #     self.player_data,
+            #     "team",
+            #     "player",
+            # )
+            # double_logging_call("Completed enriching data with TrueSkill.\n")
 
             double_logging_call("Completed enriching data with ratings.")
         except Exception as e:
